73a599f8a6a6d1d2ea19223b2f828ededae21b3e
xyugen <alex.arias.jorym@gmail.com>
2023-03-30 13:48:46 +0800

    feat(colormod.hpp): add more colors and reset

--- colormod.hpp
+++ colormod.hpp
@@ -4,11 +4,16 @@ namespace Color {
         FG_RED      = 31,
         FG_GREEN    = 32,
         FG_BLUE     = 34,
+        FG_MAGENTA  = 35,
+        FG_CYAN     = 36,
         FG_DEFAULT  = 39,
         BG_RED      = 41,
         BG_GREEN    = 42,
         BG_BLUE     = 44,
-        BG_DEFAULT  = 49
+        BG_MAGENTA  = 45,
+        BG_CYAN     = 46,
+        BG_DEFAULT  = 49,
+        RESET       = 0
     };
     class Modifier {
         Code code;
